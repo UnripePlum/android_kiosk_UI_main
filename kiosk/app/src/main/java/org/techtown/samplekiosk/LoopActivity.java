4ca3f395c526836a298e23b54217147e9e0f05e8
김한준 <khjalk8625@kaist.ac.kr>
2022-09-25 20:04:24 +0900

    20220925

--- kiosk/app/src/main/java/org/techtown/samplekiosk/LoopActivity.java
+++ kiosk/app/src/main/java/org/techtown/samplekiosk/LoopActivity.java
@@ -1,9 +1,16 @@
 package org.techtown.samplekiosk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,32 +19,39 @@ import android.widget.Button;
 import org.techtown.samplekiosk.NormalActivity.NormalActivity;
 import org.techtown.samplekiosk.OldActivity.OldActivity;
 
-public class LoopActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class LoopActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
+
+    
 
     private final int MODE_NORMAL = 0;
     private final int MODE_OLD = 1;
     private final int MODE_BLIND = 2;
     public int mode = -1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loop);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        Button buttonMODE_NORMAL = findViewById(R.id.buttonMODE_NORMAL);
-        Button buttonMODE_OLD = findViewById(R.id.buttonMODE_OLD);
 
-        buttonMODE_NORMAL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MenuStart(MODE_NORMAL);
-            }
-        });
 
-        buttonMODE_OLD.setOnClickListener(new View.OnClickListener() {
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new DevicesFragment(), "devices").commit();
+        else
+            onBackStackChanged();
+
+        Button sendbtn = findViewById(R.id.sendbtn);
+        sendbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                MenuStart(MODE_OLD);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UsbService.class);
+                intent.putExtra("send", "101");
+                startService(intent);
             }
         });
 
@@ -62,7 +76,26 @@ public class LoopActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if("android.hardware.usb.action.USB_DEVICE_ATTACHED".equals(intent.getAction())) {
+//            TerminalFragment terminal = (TerminalFragment)getSupportFragmentManager().findFragmentByTag("terminal");
+//            if (terminal != null)
+//                terminal.status("USB device detected");
+
+        }
+        super.onNewIntent(intent);
+    }
 
 
+    @Override
+    public void onBackStackChanged() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(getSupportFragmentManager().getBackStackEntryCount()>0);
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
---
+++ kiosk/app/src/main/java/org/techtown/samplekiosk/UsbService.java
@@ -0,0 +1,258 @@
+package org.techtown.samplekiosk;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class UsbService extends Service implements SerialInputOutputManager.Listener {
+    private static String STATUS = "MyService";
+    private enum UsbPermission { Unknown, Requested, Granted, Denied }
+
+    private static final String INTENT_ACTION_GRANT_USB = BuildConfig.APPLICATION_ID + ".GRANT_USB";
+    private static final int WRITE_WAIT_MILLIS = 2000;
+    private static final int READ_WAIT_MILLIS = 2000;
+
+    private int deviceId, portNum, baudRate;
+    private boolean withIoManager;
+
+    private final BroadcastReceiver broadcastReceiver;
+    private final Handler mainLooper;
+    UsbManager usbManager;
+
+
+    private SerialInputOutputManager usbIoManager;
+    private UsbSerialPort usbSerialPort;
+    private UsbPermission usbPermission = UsbPermission.Unknown;
+    private boolean connected = false;
+
+
+    public UsbService() {
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(INTENT_ACTION_GRANT_USB.equals(intent.getAction())) {
+                    usbPermission = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
+                            ? UsbPermission.Granted : UsbPermission.Denied;
+                    connect();
+                }
+            }
+        };
+        mainLooper = new Handler(Looper.getMainLooper());
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getApplicationContext().registerReceiver(broadcastReceiver, new IntentFilter(INTENT_ACTION_GRANT_USB));
+
+        if(usbPermission == UsbPermission.Unknown || usbPermission == UsbPermission.Granted)
+            mainLooper.post(this::connect);
+
+    }
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if(intent == null){
+            return Service.START_STICKY;
+        }
+        else{
+            setArgs(intent);
+            if(intent.getExtras().getString("send") != null){
+                send(intent.getExtras().getString("send"));
+            }
+
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void setArgs(Intent intent){
+        Bundle bundle = intent.getExtras();
+        deviceId = bundle.getInt("device");
+        portNum = bundle.getInt("port");
+        baudRate = bundle.getInt("baud");
+        withIoManager = bundle.getBoolean("withIoManager");
+    }
+    @Override
+    public void onNewData(byte[] data) {
+        mainLooper.post(() -> {
+            receive(data);
+        });
+    }
+
+    @Override
+    public void onRunError(Exception e) {
+        mainLooper.post(() -> {
+            status("connection lost: " + e.getMessage());
+            disconnect();
+        });
+    }
+
+    private void connect() {
+        UsbDevice device = null;
+        usbManager = (UsbManager) this.getSystemService(Context.USB_SERVICE);
+        for(UsbDevice v : usbManager.getDeviceList().values())
+        {
+            if(v.getDeviceId() == deviceId)
+            {
+
+                device = v;
+            }
+        }
+        if(device == null) {
+            status("connection failed: device not found");
+
+            return;
+        }
+        UsbSerialDriver driver = UsbSerialProber.getDefaultProber().probeDevice(device);
+        if(driver == null) {
+            driver = CustomProber.getCustomProber().probeDevice(device);
+        }
+        if(driver == null) {
+            status("connection failed: no driver for device");
+            return;
+        }
+        if(driver.getPorts().size() < portNum) {
+            status("connection failed: not enough ports at device");
+            return;
+        }
+        usbSerialPort = driver.getPorts().get(portNum);
+        UsbDeviceConnection usbConnection = usbManager.openDevice(driver.getDevice());
+        if(usbConnection == null && usbPermission == UsbPermission.Unknown && !usbManager.hasPermission(driver.getDevice())) {
+            usbPermission = UsbPermission.Requested;
+            int flags = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? PendingIntent.FLAG_IMMUTABLE : 0;
+            PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(INTENT_ACTION_GRANT_USB), flags);
+            usbManager.requestPermission(driver.getDevice(), usbPermissionIntent);
+            return;
+        }
+        if(usbConnection == null) {
+            if (!usbManager.hasPermission(driver.getDevice()))
+                status("connection failed: permission denied");
+            else
+                status("connection failed: open failed");
+            return;
+        }
+
+        try {
+            usbSerialPort.open(usbConnection);
+            usbSerialPort.setParameters(baudRate, 8, 1, UsbSerialPort.PARITY_NONE);
+            if(withIoManager) {
+                usbIoManager = new SerialInputOutputManager(usbSerialPort, this);
+                usbIoManager.start();
+            }
+
+            status("connected");
+            connected = true;
+
+        } catch (Exception e) {
+            status("connection failed: " + e.getMessage());
+            disconnect();
+        }
+    }
+
+    private void disconnect() {
+        connected = false;
+
+        if(usbIoManager != null) {
+            usbIoManager.setListener(null);
+            usbIoManager.stop();
+        }
+        usbIoManager = null;
+        try {
+            usbSerialPort.close();
+        } catch (IOException ignored) {}
+        usbSerialPort = null;
+    }
+
+    private void send(String str) {
+        if(!connected) {
+            Toast.makeText(getApplicationContext(), "not connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            byte[] data = (str + '\n').getBytes();
+            Log.d(STATUS, "send Data : " + str);
+            usbSerialPort.write(data, WRITE_WAIT_MILLIS);
+        } catch (Exception e) {
+            onRunError(e);
+        }
+    }
+
+    private void read() {
+        if(!connected) {
+            Toast.makeText(getApplicationContext(), "not connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            byte[] buffer = new byte[8192];
+            int len = usbSerialPort.read(buffer, READ_WAIT_MILLIS);
+            receive(Arrays.copyOf(buffer, len));
+        } catch (IOException e) {
+            // when using read with timeout, USB bulkTransfer returns -1 on timeout _and_ errors
+            // like connection loss, so there is typically no exception thrown here on error
+            status("connection lost: " + e.getMessage());
+            disconnect();
+        }
+    }
+
+    private void receive(byte[] data) {
+
+        String dataStr = new String(data);
+        Log.d("MyService", dataStr);
+
+    }
+
+    void status(String str) {
+
+        Log.d(STATUS, str);
+    }
+}
